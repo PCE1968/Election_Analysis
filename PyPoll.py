3057007a38395d28022a5178de3d1f846324ee0a
PCE1968 <erickson.paul@mayo.edu>
2022-01-10 00:42:08 -0600

    update write to files with Python

--- PyPoll.py
+++ PyPoll.py
@@ -9,13 +9,27 @@ import os
 
 #Assign a variable for the file to load and the path.
 file_to_load = os.path.join("Resources", "election_results.csv")
+# creat a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 #Open the election results and read the file.
 with open(file_to_load) as election_data:
 
     # to do: perform analysis.
     print(election_data)
-# creat a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+
 # Using the open() function with the "w" mode we will write data to teh file
-open(file_to_save, w)
+with open(file_to_save, "w") as txt_file:
+#write some data to the file.
+#outfile.write("Hello World")
+
+    # Write three counties to the file.
+    #txt_file.write("Arapahoe, ")
+    #txt_file.write("Denver, ")
+    #txt_file.write("Jefferson")
+
+    # Write three counties to the file.
+    txt_file.write("Counties in the Election\n-------------------------\nArapahoe\nDenver\nJefferson")
+
+# Close the file
+txt_file.close()
