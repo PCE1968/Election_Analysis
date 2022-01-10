4e42705e167dbed4db0ec93de3a1535d77476a36
PCE1968 <erickson.paul@mayo.edu>
2022-01-09 23:00:07 -0600

    added Python practice files

---
+++ Python_practice.py
@@ -0,0 +1,6 @@
+counties_dict ={'Arapahoe':422829,'Denver':463353,'Jefferson':432438}
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829}, {"county":"Denver", "registered_voters": 463353}, {"county":"Jefferson", "registered_voters": 432438}]
+
+for counties_dict in voting_data:
+    #for key, value in counties_dict.items():
+    print(f"{counties_dict['county']} county has {counties_dict['registered_voters']:,} registered voters")
