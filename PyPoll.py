13b4f8d98e43c7bb112e1008a59f8957ce5bd62c
PCE1968 <erickson.paul@mayo.edu>
2022-01-10 00:21:23 -0600

    update Python Practice

--- PyPoll.py
+++ PyPoll.py
@@ -4,14 +4,18 @@
 # 3. The percentage of votes each candidate won
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on popular vote.
+import csv
+import os
 
 #Assign a variable for the file to load and the path.
-file_to_load = 'Resources/election_results.csv'
+file_to_load = os.path.join("Resources", "election_results.csv")
 
 #Open the election results and read the file.
-election_data = open(file_to_load, 'r')
+with open(file_to_load) as election_data:
 
-# to do: perform analysis.
-
-# Close the file.
-election_data.close()
+    # to do: perform analysis.
+    print(election_data)
+# creat a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Using the open() function with the "w" mode we will write data to teh file
+open(file_to_save, w)
