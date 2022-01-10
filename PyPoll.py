a4f8c5eb78c4960c8f062b95e91ad11b4b296621
PCE1968 <erickson.paul@mayo.edu>
2022-01-09 23:55:05 -0600

    updated PyPoll

--- PyPoll.py
+++ PyPoll.py
@@ -4,3 +4,14 @@
 # 3. The percentage of votes each candidate won
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on popular vote.
+
+#Assign a variable for the file to load and the path.
+file_to_load = 'Resources/election_results.csv'
+
+#Open the election results and read the file.
+election_data = open(file_to_load, 'r')
+
+# to do: perform analysis.
+
+# Close the file.
+election_data.close()
